911d086035b88af2e369713d6ed22b5219b29d14
vaipesNL <brian.broeders@hotmail.nl>
2024-05-13 20:51:58 +0200

    dummy commit

--- Assets/Scripts/Player/Player_Controller.cs
+++ Assets/Scripts/Player/Player_Controller.cs
@@ -14,6 +14,7 @@ public class Player_Controller : MonoBehaviour
     public LayerMask groundLayer;           // Layermask for identifying ground objects
 
     //public CapsuleCollider2D cc;
+    //Comments
 
     private Rigidbody2D rb;
     private bool isGrounded;
